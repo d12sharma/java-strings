3f591a5cc34c2e5ac8f960696658c5c23334c986
dhruvsharma123 <dhruv1931.be21@chitkara.edu.in>
2025-03-06 13:52:26 +0530

    DhruvSharma add:generate exception and handling

---
+++ Generating.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Generating{
+	public static void main(String[] args){
+	
+	// generateException();
+	handleException();
+		
+	}
+	public static void generateException(){
+		String s1 = null;
+		s1.length();
+	}
+	public static void handleException(){
+		String s1=null;
+	try{
+		s1.length();
+	}
+	catch (Exception e ){
+		e.getMessage();
+	}
+	}
+}
